318b33f1b5ad9bda619bd696730997ca677336c3
Мельникова Надежда <nmelnikova@ibsys.ru>
2022-06-20 11:19:38 +0300

    menu, routing

--- pet-cat/src/app/app-routing.module.ts
+++ pet-cat/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'main',
+    loadChildren: () => import('./main/main.module').then((m) => m.MainModule) },
+  {
+    path: 'statistic',
+    loadChildren: () => import('./statistic/statistic.module').then((m) => m.StatisticModule),
+  },
+  {
+    path: 'form',
+    loadChildren: () => import('./form/form.module').then((m) => m.FormModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
